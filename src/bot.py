936f1ae78c75bcf1966ee8a8f5b56d413b4bf176
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-19 14:03:47 +0400

    Changed channels

--- src/bot.py
+++ src/bot.py
@@ -691,8 +691,8 @@ The code here isn't included in config.json
 
 @tasks.loop(hours=2)
 async def scheduled_ping():
-    channel = bot.get_channel(1454853658758873251)
+    channel = bot.get_channel(1422933244378222642)
     if channel:
-        await channel.send(f"Bump edin. <@&{1454851358955016244}>")
+        await channel.send(f"Bump edin. <@&{1457349381127667869}>")
 
 bot.run(discord_token)
